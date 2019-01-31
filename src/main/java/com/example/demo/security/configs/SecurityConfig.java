be6a4a1b615d3e69351cae147fe2c59600a385dc
Norayr Gharibyan [a739439] <norayrgh@energizeglobal.com>
2019-01-31 18:47:37 +0400

    first commit

---
+++ src/main/java/com/example/demo/security/configs/SecurityConfig.java
@@ -0,0 +1,14 @@
+package com.example.demo.security.configs;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/users/**").authenticated();
+    }
+}
